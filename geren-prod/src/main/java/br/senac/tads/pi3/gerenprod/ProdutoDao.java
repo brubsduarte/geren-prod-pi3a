adc9c95b91b0a83c2eba68690b4d70316fd91028
gpereira62 <gpereira62@github.com>
2019-03-10 00:26:05 -0300

    Criado parte do javaDoc do Metodo Excluir da Classe: ProdutoDAO

--- geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/ProdutoDao.java
+++ geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/ProdutoDao.java
@@ -46,7 +46,12 @@ public class ProdutoDao {
                 + "DISPONIVEL = '"+p.isProdutoDisponivel()+";";
         return db.executarAlteracao(sql);
     }
-
+    
+     /**
+     * 
+     * @param produtoID
+     * @return 
+     */
     public static boolean Excluir(int produtoID) {
         DB db = new DB(true);
         String sql = "DELETE FROM produto Where ID = "+produtoID+";";
